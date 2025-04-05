7ed9c166c8f4e628dd85de5ee5acf07e7bd17430
GamerPC <GamerPC@DESKTOP-K7RKTVQ>
2025-04-05 18:28:22 +0800

    SeperationInProgress

--- FunctionsWithArrays/functionWithArrays.cpp
+++ FunctionsWithArrays/functionWithArrays.cpp
@@ -1,19 +1,11 @@
-﻿#include<iostream>
-using namespace std;
+﻿#include"stdafx.h"
+#include"constants.h"
+#include"fillRand.h"
+//#iclude"fillRand.cpp" // Реализации функции не подключчать на место вызова
+#include"Print.h"
 
-#define delimiter "\n--------------------------------------\n"
-const int ROWS = 4;
-const int COLS = 5;
 
-void FillRand(double arr[], const int n);
-void FillRand(int arr[], const int n);
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand = 0, int maxRand = 100);
-void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS);
 
-void Print(double arr[], const int n);
-void Print(int arr[], const int n);
-void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
-void Print(double arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void Sort(double arr[], const int n);
 void Sort(int arr[], const int n);
@@ -119,85 +111,11 @@ void main()
 	Print(brr, SIZE);
 }
 //_____________________________________________________
-void FillRand(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 100;
-	}
-}
-void FillRand(double arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 10000;
-		arr[i] /= 100;
-	}
-}
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand, int maxRand)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand() % (maxRand - minRand) + minRand;
-		}
-	}
-}
-void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			arr[i][j] = rand() % 10000;
-			arr[i][j] /= 100;
-		}
-	}
-}
 
 
 
-void Print(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
-void Print(double arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-}
-void Print(int arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
-void Print(double arr[ROWS][COLS], const int ROWS, const int COLS)
-{
-	for (int i = 0; i < ROWS; i++)
-	{
-		for (int j = 0; j < COLS; j++)
-		{
-			cout << arr[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
+
+
 
 
 void Sort(int arr[], const int n)
